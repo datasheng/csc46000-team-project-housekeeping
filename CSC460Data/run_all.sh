70a2297a8a9953ce82c38ad29d3e26dd8713db7c
FD-77 <163379162+FD-77@users.noreply.github.com>
2025-12-07 14:01:19 -0500

    Update script paths in run_all.sh

--- CSC460Data/run_all.sh
+++ CSC460Data/run_all.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-cd AllScripts
-python3 consumptionRates.py
-python3 propertyTaxData.py
-python3 clean.py
-python3 mysqlConnections.py
+python3 AllScripts/consumptionRates.py
+python3 AllScripts/propertyTaxData.py
+python3 AllScripts/clean.py
+python3 AllScripts/mysqlConnections.py
