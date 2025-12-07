aeff2d96a526b2679b6f82e93fd4476f77eb0f66
FD-77 <163379162+FD-77@users.noreply.github.com>
2025-12-07 14:04:31 -0500

    Delete CSC460Data directory

--- CSC460Data/AllScripts/avg.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-df=pd.read_csv("BackupData/nysData.csv")
-df['total']=(df['Food']+df['Medical']+df['Transportation'] + df['Civic'] + df['Internet & Mobile'] +df['Other'] + df['Annual taxes'] )
-print(df)
--- CSC460Data/run_all.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-python3 AllScripts/consumptionRates.py
-python3 AllScripts/propertyTaxData.py
-python3 AllScripts/clean.py
-python3 AllScripts/mysqlConnections.py
